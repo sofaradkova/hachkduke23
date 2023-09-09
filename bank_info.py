5fbe5fc8b4f8469a132a4a9dc0b06e9949aa8837
James Huang <james@Huang-Precision5550.(none)>
2023-09-09 17:44:13 -0500

    adding to dev

--- bank_info.py
+++ bank_info.py
@@ -1,12 +1,38 @@
 import json
-from datetime import date
+from datetime import date, timedelta, datetime
+
+
+# Define a function to filter transactions within the past 30 days
+def filter_transactions(transactions):
+    today = date.today()
+    thirty_days_ago = today - timedelta(days=30)
+
+    filtered_transactions = []
+
+    for transaction in transactions:
+        date_transacted = datetime.strptime(transaction["date_transacted"], "%Y-%m-%d").date()
+        if thirty_days_ago <= date_transacted <= today:
+            filtered_transactions.append(transaction)
+
+    return filtered_transactions
 
 with open('transactions.json') as f:
     business_account = json.load(f)
 
-    print(business_account["override_accounts"][0]["type"])
+    # Get transactions for the first account
+    checking_transactions = business_account["override_accounts"][0]["transactions"]
+    filtered_checking_transactions = filter_transactions(checking_transactions)
 
+    # Get transactions for the second account
+    savings_transactions = business_account["override_accounts"][1]["transactions"]
+    filtered_savings_transactions = filter_transactions(savings_transactions)
+    print("Checking Account Transactions in the Last 30 Days:")
+    for transaction in filtered_checking_transactions:
+        print(transaction)
 
+    print("\nSavings Account Transactions in the Last 30 Days:")
+    for transaction in filtered_savings_transactions:
+        print(transaction)
 class BankInfo:
     """
     This is the class that has all the information of the user's bank statement
