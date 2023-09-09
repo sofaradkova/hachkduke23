e123d2457ad342c22acc36ee0e087300211be4a4
apramodk <apramodk@vols.utk.edu>
2023-09-09 12:26:06 -0400

    boiler plate, daisyui/tailwindcss added to base sveltekit skeleton

--- svelte.config.js
+++ svelte.config.js
@@ -4,6 +4,10 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 const config = {
   kit: {
     adapter: adapter(),
+    alias: {
+      $lib: './src/lib',
+      $components: './src/lib/components',
+    },
   },
   preprocess: vitePreprocess(),
 };
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,19 @@
 module.exports = {
     content: ['./src/routes/**/*.{svelte,js,ts}'],
     plugins: [require('daisyui')],
+    daisyui: {
+        themes: [
+            {
+            mytheme: {
+            "primary": "#18314F", 
+            "primary-focus": "#0D0630",
+            "secondary": "#8BBEB2",
+            "secondary-focus": "#384E77",
+            "accent": "#E6F9AF",
+            "neutral": "#FFFFFF",
+            "base-100": "#3D3D3D",
+            },
+        },
+        ],
+    },
 };
