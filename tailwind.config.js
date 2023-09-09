8968388129f8c1f0741bfddc9bbace7cdbbf36e5
apramodk <apramodk@vols.utk.edu>
2023-09-09 15:39:48 -0400

    done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,22 @@
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [require('daisyui')],
-  daisyui: {
-    themes: [
-      {
-      mytheme : {
-        "primary": "#18314F", 
-        "primary-focus": "#0D0630",
-        "secondary": "#8BBEB2",
-        "secondary-focus": "#384E77",
-        "accent": "#E6F9AF",
-        "neutral": "#FFFFFF",
-        "base-100": "#3D3D3D",
-        },
-      }
-    ],
-  },
-}
+	content: ["./src/**/*.{html,js,svelte,ts}"],
+	theme: {
+		extend: {},
+	},
+	plugins: [require("daisyui")],
+	daisyui: {
+		themes: [
+			{
+				mytheme: {
+					primary: "#18314F",
+					"primary-focus": "#0D0630",
+					secondary: "#8BBEB2",
+					"secondary-focus": "#384E77",
+					accent: "#E6F9AF",
+					neutral: "#FFFFFF",
+					"base-100": "#3D3D3D",
+				},
+			},
+		],
+	},
+};
