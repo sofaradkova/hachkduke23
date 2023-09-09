988cf6bc90a4a0200fbd8f1bfe36dbad920a0612
apramodk <apramodk@vols.utk.edu>
2023-09-09 13:46:08 -0400

    fixed issue w daisyui -> svelte interaction

--- svelte.config.js
+++ svelte.config.js
@@ -2,14 +2,10 @@ import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 const config = {
-  kit: {
-    adapter: adapter(),
-    alias: {
-      $lib: './src/lib',
-      $components: './src/lib/components',
-    },
-  },
-  preprocess: vitePreprocess(),
+    preprocess: vitePreprocess(),
+    kit: {
+        adapter: adapter()
+    }
 };
 
 export default config;
