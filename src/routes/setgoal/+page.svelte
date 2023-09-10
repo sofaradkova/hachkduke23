ca967024974b1bac47d0d9bd8a4ce348e1a5616f
sofaradkova <123261570+sofaradkova@users.noreply.github.com>
2023-09-10 08:01:42 -0400

    fix form for goal setting

--- src/routes/setgoal/+page.svelte
+++ src/routes/setgoal/+page.svelte
@@ -1,16 +1,30 @@
 <script>
-    function redirectToNewPage() {
+    let goalValue = '';
+    let timeValue = '';
+
+  function handleSubmit() {
+    console.log("submitted")
     window.location.href = '/setbudget';
   }
 </script>
 
 <div class="flex flex-col items-center justify-center bg-white min-h-screen p-4 text-gray-800" >
     <h1 class="text-4xl font-bold mb-6">Let's get specific!</h1>
-    <div class="w-[300px]">
-        <h2 class="self-start">How much do you want to save?</h2>
-    <input type="text" placeholder="Amount" class="input input-bordered w-full max-w-xs mb-4" />
-    <h2 class="self-start">What is your goal time?</h2>
-    <input type="text" placeholder="Deadline" class="input input-bordered w-full max-w-xs mb-4" />
-    </div>
-    <button class="btn bg-[#0D0630] text-white mt-2" on:click={redirectToNewPage}>Set budget</button>
+    <form on:submit={handleSubmit} class="flex flex-col items-center">
+        <div class="w-[300px]">
+        <label for="goal" class="self-start">How many $ do you want to save?</label>
+        <input type="number" placeholder="Amount" class="input input-bordered w-full max-w-xs mb-4 bg-white border border-primary" id="goal" bind:value={goalValue}
+        required/>
+        <label for="time" class="self-start">What is your goal time?</label>
+        <select class="select select-bordered w-full max-w-xs bg-white border border-primary mb-4" id="time" bind:value={timeValue} required >
+            <option disabled selected>Deadline</option>
+            <option value="1">1 month</option>
+            <option value="2">2 months</option>
+            <option value="3">6 months</option>
+            <option value="4">9 months</option>
+            <option value="5">1 year</option>
+        </select>
+        </div>
+        <button class="btn bg-[#0D0630] text-white mt-2" type="submit">Set budget</button>
+    </form>
 </div>
